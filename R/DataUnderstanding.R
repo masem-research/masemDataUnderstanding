646ce6eb4d18e6c3eb9da601c50e41768464e0ba
masem-research <67373640+masem-research@users.noreply.github.com>
2020-07-08 13:04:07 +0200

    Added function freqTable()

--- R/DataUnderstanding.R
+++ R/DataUnderstanding.R
@@ -29,5 +29,38 @@ numberedVariablesList <- function(df) {
 
 
 
+#' Frequency table with number of cases and relative number of cases (%)
+#'
+#' @description Provides a table with absolute and relative number of cases.
+#' The output table can be easily used in Markdown with kable().
+#' @param x vector. Should be a vector.
+#'
+#' @return data.frame. absolute and relative values
+#' @export
+#'
+#' @examples
+#' ## iris
+#' freqTable(x = iris$Species)
+#' ## mtcars
+#' # automatic or manual?
+#' freqTable(x = mtcars$am)
+#' # number of gears
+#' freqTable(x = mtcars$gear)
+freqTable <- function(x) {
+  # generate absolute and relative frequencies
+  table.absolute.values <- table(x)
+  table.relative.values <- prop.table(table.absolute.values)*100
+  # combine both variables
+  df.absolute.and.relative.values <- data.frame(table.absolute.values)
+  df.absolute.and.relative.values$p <- table.relative.values
+  # set column names
+  names(df.absolute.and.relative.values) <- c("value","n","p [%]")
+  # return df
+  return(df.absolute.and.relative.values)
+}
+
+
+
+
 
 

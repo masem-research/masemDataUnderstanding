977376d36e6709e6f627db19e6a77df4d0826acf
masem research institute <67373640+masem-research@users.noreply.github.com>
2021-02-05 15:27:20 +0100

    Added round2() function

--- R/UnderDevelopment.R
+++ R/UnderDevelopment.R
@@ -11,3 +11,18 @@ load.package <- function(x)
 
 # Load the fine mlr3 framework
 load.package("mlr3")
+
+
+
+
+## Function to round like we learned it in school :-)
+#   0.5 will be rounded up to 1
+#   (round() function in R will round 0.5 to 0)
+round2 = function(x, n) {
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5 + sqrt(.Machine$double.eps)
+  z = trunc(z)
+  z = z/10^n
+  z*posneg
+}

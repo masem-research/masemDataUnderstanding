b3cfcd8e3e16e81369f90068f38fa9de6711f1ab
masem research institute <67373640+masem-research@users.noreply.github.com>
2022-03-24 15:35:54 +0100

    Funktion BerechnungTestStatistiken() hinzugefügt
    Funktion BerechnungTestStatistiken() hinzugefügt

--- R/UnderDevelopment.R
+++ R/UnderDevelopment.R
@@ -26,3 +26,36 @@ round2 = function(x, n) {
   z = z/10^n
   z*posneg
 }
+
+
+#' Berechnung einiger zentraler Evaluationsparameter: Vorhersage Metrischer Variablen
+#'
+#' @param Labels numeric. Vektor der metrisch skalierten Zielvariablen. NA-Werte sind aktuell nicht erlaubt und 
+#' müssen noch manuell entfernt werden
+#' @param Predictions numeric. Vektor der auf Basis des Machine Learning Models vorhergesagten 
+#' metrisch-skalierten Variablen
+#'
+#' @return character. Named Vektor mit zentralen Evaluationsparametern: MAE, MSE, RMSE und R2
+#' @export
+#'
+#' @examples
+#' BerechnungTestStatistiken(Labels = c(0.00, 1.50, 3.50, 6.30, 1.50, 0.00, 3.00, 4.16),
+#'                           Predictions = c(0.1653140, 2.8294117, 3.5218980, 5.0807042, 2.9414713, 
+#'                                          -0.1564419, 3.0363548, 2.4665890))
+BerechnungTestStatistiken <- function(Labels, Predictions) {
+  # Residuen berechnen
+  Residuals <- Labels - Predictions
+  # MSE berechnen:
+  mse <-  mean((Residuals)^2)
+  # MAE  berechnen:
+  mae <- mean(abs(Residuals))
+  # RMSE berechnen:
+  rmse <- sqrt(mse)
+  # R2 berechnen:
+  R2 <- 1-(sum((Residuals)^2)/sum((Labels - mean(Labels))^2))
+  # Werte auf der Konsole ausgeben:
+  cat(" MAE:", mae, "\n", "MSE:", mse, "\n", "RMSE:", rmse, "\n", "R-squared:", R2, "\n\n")
+  # Ausgabevektor erzeugen:
+  return(c(MAE = mae, MSE = mse, RMSE = rmse, R2 = R2))
+  
+}

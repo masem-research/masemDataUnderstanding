b2a6432280ced40057ca4c31c23ffa96d35d1932
masem research institute <67373640+masem-research@users.noreply.github.com>
2022-03-25 09:40:19 +0100

    Funktion ResiduenBereinigen() zur Bereinigung eines lm-Objekts aus Basis stand. Residuen erstellt
    Funktion erstellt, mit Beispiel versehen.

--- R/UnderDevelopment.R
+++ R/UnderDevelopment.R
@@ -59,3 +59,62 @@ BerechnungTestStatistiken <- function(Labels, Predictions) {
   return(c(MAE = mae, MSE = mse, RMSE = rmse, R2 = R2))
   
 }
+
+
+
+#' Entfernt standardisierte Residuen eines lm-Objekts außerhalb des angegebenen Schwellenwertes 
+#'
+#' @description Die Funktion hat als Parameter ein lm-Objekt und einen Schwellenwert als z-Wert
+#' Zuerst werden die vorhandenen Residuen des lm-Objekts z-standardisiert grafisch darstellt, dann werden
+#' Fälle außerhalb des angegebenen Schwellenwertes identifiziert und eine um diese Fälle bereinigte Grafik
+#' angezeigt. Es wird ein Filter mit den außerhalb liegenden Fällen als TRUE zurückgegeben.
+#'
+#' @param lmObjekt lm. lm-Objekt als Ergebnis der lm()-Funktion. 
+#' @param SchwellenwertAusschlussStandResiduen numerisch. Schwellenwert in z-Einheiten. default ist 2.
+#'
+#' @return logical. Logischer Vektor, TRUE stellt einen Wert außerhalb des Schwellenwertes vor. Kann als Filter zur 
+#' Bereinigung des  data.frame genutzt werden.
+#' 
+#' @export
+#'
+#' @examples
+#' mtcarsLMObjekt <- lm(mpg ~ ., data = mtcars)
+#' ResiduenBereinigen(lmObjekt = mtcarsLMObjekt, SchwellenwertAusschlussStandResiduen = 2)
+ResiduenBereinigen <- function(lmObjekt, SchwellenwertAusschlussStandResiduen) {
+  ## lm-Objekt?
+  if (!inherits(x = lmObjekt, what = "lm")) {
+    stop("Error: Anscheinend liegt kein lm-Objekt vor.")
+  }
+  ## Numerische Wert für Schwellenwert?
+  if (!is.numeric(x = SchwellenwertAusschlussStandResiduen)) {
+    stop("Error: Der Schwellenwert ist als numerischer Wert anzugeben.")
+  }
+  ## Grafik-Fenster vorbereiten: 2 Spalten, 1 Zeile
+  par(mfrow = c(1, 2))
+  ## Standardisierte Residuen erzeugen
+  zStandResiduen <- scale(resid(lmObjekt))
+  ## Plot
+  hist(x = zStandResiduen, col = "orange", freq = FALSE, 
+       main = "z-Stand. Residuen", xlab = "z-Werte")
+  lines(density(zStandResiduen), lty = 2)
+  ## Schwellenwerte nach Parameter einzeichnen
+  abline(v = c(-1*abs(SchwellenwertAusschlussStandResiduen), abs(SchwellenwertAusschlussStandResiduen)), lty= 2)
+  ## Filter erstellen: stand. Residuen > Schwellenwert
+  FilterStandResiduenSchwellenwert <- abs(zStandResiduen) > abs(SchwellenwertAusschlussStandResiduen)
+  ## Statistik:
+  message(paste0("Es liegen ", sum(FilterStandResiduenSchwellenwert), 
+                " Fälle von insgesamt ", length(FilterStandResiduenSchwellenwert),
+                " außerhalb des angegebenen Schwellenwertes."))
+  ## Plot ohne ausgeschlossene Fälle
+  hist(x = zStandResiduen[!FilterStandResiduenSchwellenwert], col = "yellow", freq = FALSE, 
+       main = "z-Stand. Residuen", xlab = "z-Werte")
+  lines(density(zStandResiduen[!FilterStandResiduenSchwellenwert]), lty = 2)
+  ## Grafik-Fenster wieder zurücksetzen
+  par(mfrow = c(1, 1))
+  
+  ## Filter zurückgeben
+  message("Gebe Filter zurück - Werte außerhalb des Schwellenwertes sind TRUE")
+  return(FilterStandResiduenSchwellenwert)
+  
+}
+
